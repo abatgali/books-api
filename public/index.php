cce39b8c6c0ceb31e73dd072eaa3f38e40715ac1
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-05-20 14:04:18 -0400

    Folder Structure for API integration and database linkage.

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Author:Adam Wright
+ * Date: 5/20/2022
+ * File:Index
+ */
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate app
+$app = AppFactory::create();
+//Set basepath
+$app->setBasePath('/I425/mycollege-api');
+// Add middleware for parsing JSON, form data and xml
+$app->addBodyParsingMiddleware();
+// Add the Slim built-in routing middleware
+$app->addRoutingMiddleware();
+
+// Add Error Handling Middleware
+$app->addErrorMiddleware(true, true, true);
+// Define app route
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('Welcome to MyCollege API!');
+    return $response;
+});
+// Run an app route
+$app->run();
