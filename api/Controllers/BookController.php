c3386f557d1468f52babb9adb7486903fa0becf6
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-05-23 11:44:25 -0400

    fix to routes
    Routes fix

---
+++ api/Controllers/BookController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Author: "Emma Parker"
+ * Date: 5/22/22
+ * File: BookController.php
+ *Description: define the book controller class
+ */
+
+namespace BooksAPI\Controllers;
+
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use BooksAPI\Models\Book;
+use BooksAPI\Controllers\ControllerHelper as Helper;
+class BookController {
+    //list all books
+    public function index(Request $request, Response $response, array $args) : Response {
+        $results = Book::getBooks();
+        return Helper::withJson($response, $results, 200);
+    }
+
+    public function view(Request $request,Response $response, array $args) : Response {
+        $book_id = $args['book_id'];
+        $results = Book::getBookById($book_id);
+       return Helper::withJson($response, $results, 200);
+    }
+
+}
