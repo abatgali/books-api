0ebbc6d0224652a4428c6f56fc936e54cd23a723
OluwatisheElesi <61428469+OluwatisheElesi@users.noreply.github.com>
2022-06-01 19:38:17 -0400

    Update Author.php

--- api/Models/Author.php
+++ api/Models/Author.php
@@ -8,7 +8,7 @@
  */
 
 
-namespace BooksAPI\Models;
+amespace BooksAPI\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
@@ -26,30 +26,132 @@ class Author extends Model
     public $timestamps = false;
 
 //retrieve all authors
-    public static function getAuthors()
+    public static function getAuthors($request)
     {
+//        -----Canceled out following code to ake room for pagination & sorting -----
 //retrieve all authors
-        $authors = self::all();
-        return $authors;
+//        $authors = self::all();
+//        $authors = self::with("books")->get($request);
+//        return $authors;
+//        /*********** code for pagination and sorting *************************/
+        //get the total number of row count
+        $count = self::count();
+
+        //Get querystring variables from url
+        $params = $request->getQueryParams();
+
+        //do limit and offset exist?
+        $limit = array_key_exists('limit', $params) ? (int)$params['limit'] : 5;   //items per page
+        $offset = array_key_exists('offset', $params) ? (int)$params['offset'] : 0;  //offset of the first item
+
+        //pagination
+        $links = self::getLinks($request, $limit, $offset);
+
+        //build query
+        $query = self::with('books');  //build the query to get all authors
+        $query = $query->skip($offset)->take($limit);  //limit the rows
+
+
+        //code for sorting
+        $sort_key_array = self::getSortKeys($request);
+
+        //soft the output by one or more columns
+        foreach ($sort_key_array as $column => $direction) {
+            $query->orderBy($column, $direction);
+        }
+
+
+        //retrieve the authors
+        $authors = $query->get();  //Finally, run the query and get the results
+
+        //construct the data for response
+        $results = [
+            'totalCount' => $count,
+            'limit' => $limit,
+            'offset' => $offset,
+            'links' => $links,
+            'sort' => '$sort_key_array',
+            'data' => $authors,
+        ];
+
+        return $results;
+
     }
 
     public static function getAuthorById(string $id)
     {
         $author = self::findOrFail($id);
+        $author->load("");
         return $author;
 
     }
+//--------
+    // Return an array of links for pagination. The array includes links for the current, first, next, and last pages.
+    private static function getLinks($request, $limit, $offset) {
+        $count = self::count();
 
+        // Get request uri and parts
+        $uri = $request->getUri();
+        if($port = $uri->getPort()) {
+            $port = ':' . $port;
+        }
+        $base_url = $uri->getScheme() . "://" . $uri->getHost() . $port . $uri->getPath();
+
+        // Construct links for pagination
+        $links = [];
+        $links[] = ['rel' => 'self', 'href' => "$base_url?limit=$limit&offset=$offset"];
+        $links[] = ['rel' => 'first', 'href' => "$base_url?limit=$limit&offset=0"];
+        if ($offset - $limit >= 0) {
+            $links[] = ['rel' => 'prev', 'href' => "$base_url?limit=$limit&offset=" . $offset - $limit];
+        }
+        if ($offset + $limit < $count) {
+            $links[] = ['rel' => 'next', 'href' => "$base_url?limit=$limit&offset=" . $offset + $limit];
+        }
+        $links[] = ['rel' => 'last', 'href' => "$base_url?limit=$limit&offset=" . $limit * (ceil($count / $limit) - 1)];
+
+        return $links;
+    }
+    //------
+    /*
+     * Sort keys are optionally enclosed in [ ], separated with commas;
+     * Sort directions can be optionally appended to each sort key, separated by :.
+     * Sort directions can be 'asc' or 'desc' and defaults to 'asc'.
+     * Examples: sort=[number:asc,title:desc], sort=[number, title:desc]
+     * This function retrieves sorting keys from uri and returns an array.
+    */
+    private static function getSortKeys($request) {
+        $sort_key_array = [];
+
+        // Get querystring variables from url
+        $params = $request->getQueryParams();
+
+        if (array_key_exists('sort', $params)) {
+            $sort = preg_replace('/^\[|\]$|\s+/', '', $params['sort']);  // remove white spaces, [, and ]
+            $sort_keys = explode(',', $sort); //get all the key:direction pairs
+            foreach ($sort_keys as $sort_key) {
+                $direction = 'asc';
+                $column = $sort_key;
+                if (strpos($sort_key, ':')) {
+                    list($column, $direction) = explode(':', $sort_key);
+                }
+                $sort_key_array[$column] = $direction;
+            }
+        }
+
+        return $sort_key_array;
+    }
+
+
+//Get an author's books
+    public static function getAuthorBooks(string $id) {
+        $books = self::findOrFail($id)->books;
+        return $books;
+    }
     // Define the many-to-many relationship between Books and Author model classes.
     public function books(){
         return $this->belongsToMany(Book::class, 'author_and_book', 'book_id', 'author_id');
     }
 
-    //Get an author's books
-    public static function getAuthorBooks(string $id) {
-        $books = self::findOrFail($id)->books;
-        return $books;
-    }
 
     //search for authors
     public static function searchAuthors($term){
@@ -128,4 +230,5 @@ class Author extends Model
     }
 
 
+}
 }
