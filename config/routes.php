2837deaf0d83ebe4b6b0b78ab0729438d16b4faa
abatgali <abatgali@iu.edu>
2022-06-15 18:29:04 -0400

    to access resources from an SPA

--- config/routes.php
+++ config/routes.php
@@ -21,6 +21,19 @@ use BooksAPI\Authentication\{
 
 return function(App $app){
 
+    //Set up CORS (Cross-Origin Resource Sharing) https://www.slimframework.com/docs/v4/cookbook/enable-cors.html
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
+
     // Define app route
     $app->get('/', function (Request $request, Response $response, array $args) {
         $response->getBody()->write('Welcome to Books API!');
